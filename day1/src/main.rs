7da735a5d76067af718c00378252693f6a399340
knulphaller <knulphaller@gmail.com>
2022-12-16 03:19:33 +0400

    Playing with vector and map

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,14 +1,15 @@
-
 fn main() {
-    let mut max = 0;
     let input = std::fs::read_to_string("src/input.txt").unwrap();
-    for line in input.split("\n\n") {
-        let mut sum = 0;
-        for l in line.lines() {
-            let n: i32 = l.parse().unwrap();
-            sum += n;
-        }
-        max = if sum > max { sum } else { max };
-    }
+    let a: Vec<i32> = input.split("\n\n")
+        .into_iter()
+        .map(|l| l.lines()
+            .into_iter()
+            .map(|p| p.parse().unwrap())
+            .collect::<Vec<i32>>()
+            .iter()
+            .sum())
+        .collect::<Vec<i32>>();
+
+    let max = &a.iter().max().unwrap();
     println!("Max: {max}")
 }
